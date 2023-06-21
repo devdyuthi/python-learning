e19f53f74841ff0e2f57348eeff271262f407e30
Devna <devnabandarupalli28@gmail.com>
2023-06-21 14:33:45 -0500

    June 21 py learning

---
+++ python-with-nana/conditionals-and-booleans.py
@@ -0,0 +1,22 @@
+calc_to_units = 24
+name_of_unit = "hours"
+
+
+def day_to_unit(num_of_days):
+    return f"{num_of_days} days are {num_of_days * calc_to_units} {name_of_unit}"
+
+
+user_input = input(">>enter a number of days to be converted to hours\n")
+
+# using conditionals
+try:
+    if int(user_input) > 0:
+        calc_value = day_to_unit(int(user_input))
+        print(calc_value)
+    elif int(user_input) == 0:
+        print("sorry! number of days cannot be 0 :(")
+    else:
+        print("sorry! number of days must be a positive value :(")
+
+except:
+    print("sorry! your input is invalid ")
